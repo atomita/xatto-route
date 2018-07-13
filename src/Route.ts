f9ab49d3b48df1303190f47cb72c498e37853d79
atomita <sleeping.cait.sith+gh@gmail.com>
2018-07-13 19:45:39 +0900

    fix: bug

--- src/Route.ts
+++ src/Route.ts
@@ -1,19 +1,19 @@
 export function Route({ xa: { context, extra }, ...attrs }, children) {
   const location = attrs.location || context.location || extra.location || window.location
 
-  let flags = attrs.flags || null
+  let flags = attrs.flags || ''
   let pattern: RegExp | string | undefined = attrs.pattern
 
   if (null == pattern) {
     pattern = ''
   } else if (pattern instanceof RegExp) {
-    flags = flags || (pattern as any).flags || null
+    flags = flags || (pattern as any).flags || ''
     pattern = pattern.source
   }
 
   pattern = pattern[0] === '^' ? pattern.slice(1) : pattern
   pattern = pattern[0] === '/' ? pattern.slice(1) : pattern
-  pattern = new RegExp(`^/${pattern}`, flags)
+  pattern = new RegExp(`^/${pattern}`)
 
   const match: any = pattern.exec(location.pathname)
 
