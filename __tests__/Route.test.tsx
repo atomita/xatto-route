d543256341d330bd00e05ba50ae24ff06a0df101
atomita <sleeping.cait.sith+gh@gmail.com>
2018-07-14 00:15:59 +0900

    test: Add dummy

---
+++ __tests__/Route.test.tsx
@@ -0,0 +1,13 @@
+import * as assert from 'power-assert'
+
+import { x } from 'xatto'
+
+import { Route } from '../src/Route'
+
+describe('Route Component', () => {
+
+  test('dummy', () => {
+    assert(true)
+  })
+
+})
